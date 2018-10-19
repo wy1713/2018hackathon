5a257def8c4fc40f91592a6060e42ff7faafa8e2
wy1713 <36648934+wy1713@users.noreply.github.com>
2018-10-19 13:30:02 -0700

    server side socketio implementation

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+
+import socketIOClient from "socket.io-client";
 
 export default class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+        endpoint: "http://127.0.0.1:8080"
+      };
+    }
+
   state = { username: null };
 
   componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    //listen event
+    socket.on("dataArrive", data=>
+      this.setState({
+        username:data
+      })
+    )
     fetch('/api/getUsername')
       .then(res => res.json())
       .then(user => this.setState({ username: user.username }));
@@ -16,7 +32,7 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const os = require('os');
-
+//need express for server rendering
 const app = express();
+const server = require('http').createServer(app);
+//set the socket.io
+const io = require('socket.io')(server);
+
+const getLatestData = (socket)=>{
+  //replace this with server callback
+  let data = Math.random();
+
+  socket.emit("dataArrive", data);
+}
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+server.listen(8080, () => console.log('Listening on port 8080!'));
+
+io.on("connection", socket => {
+  console.log("New client connected"+socket.id);
+  //fire event
+  setInterval(()=> getLatestData(socket), 1000);
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
