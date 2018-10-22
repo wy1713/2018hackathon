b63137d6b5480fd228da8a6392c43cffa4f7ebf7
jasonchenliang <jasonchenliang@gmail.com>
2018-10-22 13:35:48 -0700

    add google function url

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rp = require('request-promise');
 const os = require('os');
 //need express for server rendering
 const app = express();
@@ -11,29 +12,26 @@ const getLatestData = (socket)=>{
   let data = Math.random();
 
   socket.emit("dataArrive", data);
-}
-
-function getList () {
-  const n = 20000;
-  var list = [];
-  for (let i = 0; i < n; i += 1) {
-    list.push({name: "user" + (i+1)});
-  }
-  return list;
-}
+};
 
 function getHtml (list) {
   var content = '<div style="text-align: center"> <h1>SSR (Server Side Rendering)</h1><h1>List</h1><ul>';
-  list.map(function (i) {
+  list.list.map(function (i) {
     content += '<li>' + i.name + '</li>';
   });
   content += '</ul></div>';
   return content;
 }
 
+const url = "https://us-central1-api-project-844570496919.cloudfunctions.net/getList";
 app.use(express.static('dist'));
-app.get('/ssr', (req, res) => res.send(getHtml(getList())));
-app.get('/api/getList', (req, res) => res.send({ list: getList() }));
+app.get('/ssr', (req, res) => rp(url).then(function(result){
+  res.send(getHtml(JSON.parse(result)));
+}));
+app.get("/api/getList", (req, res) => rp(url).then(function (result) {
+    res.send(JSON.parse(result));
+}));
+
 server.listen(8080, () => console.log('Listening on port 8080!'));
 
 io.on("connection", socket => {
