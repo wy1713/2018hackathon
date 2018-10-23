9006ce1a957655b79697d9e6e79ed2f81dc0ac9b
wayne.shi <wayne.shi@ze.com>
2018-10-22 17:30:38 -0700

    list of users -14

--- src/client/Html.js
+++ src/client/Html.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 
-const Html = ({ list }) => <div>
+const Html = ({ list }) => <div><a href="http://localhost:8080/ssr">SSR (Server Side Rendering)</a>
           <h1>CSR (Client Side Rendering)</h1>
           {list ? <h1>List</h1> : <h1>Loading.. please wait!</h1>}
           {list ? (
