b7d2cf1988c2d0a385859b49b8a2ba85dcb6c747
wayne.shi <wayne.shi@ze.com>
2018-10-23 09:01:27 -0700

    list of users -18

--- src/client/Html.js
+++ src/client/Html.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Html = ({ list }) => <div><a href="http://localhost:8080/ssr">SSR (Server Side Rendering)</a>
           <h1>CSR (Client Side Rendering)</h1><h2>- React, Next, Express, Node.js, FAAS</h2>
-          {list ? <h1>List (20,000 Items from Serverless Cloud FAAS)</h1> : <h1>Loading.. please wait!</h1>}
+          {list ? <h2>List (20,000 Items from Serverless Cloud FAAS)</h2> : <h1>Loading.. please wait!</h1>}
           {list ? (
                     <ul>
                       {list.map(i => <li key={i.name}>{i.name}</li>)}
